95eb3a36120a946fb2c0c81f5b99e6eac7ba0c69
gammahazard <dhaliswag777@gmail.com>
2026-05-28 01:06:22 -0400

    feat: add favicon, OG image, robots, and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://gammahazard.github.io',
   base: '/',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
